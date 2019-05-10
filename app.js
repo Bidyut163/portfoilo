daeedb614737739f41fcf055f566610349397654
Bidyut Deka <bideka03@in.ibm.com>
2019-05-10 21:14:24 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,104 @@
+function popup(projectNo) {
+  if (projectNo === 1) {
+    document.querySelector(".popup__img").src = "./img/popup-1.png";
+
+    document.querySelector(".popup__title").innerHTML = "Sochlo.in";
+
+    document.querySelector(".popup__tagline").innerHTML = "A blog site";
+
+    document.querySelector(".popup__para").innerHTML =
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero,accusamus sequi. Eius totam impedit illum quasi! Dolor pariatur, corporis quibusdam eius unde aliquam possimus nemo! lorem!!";
+
+    document.querySelector("#site-link").href = "http://www.sochlo.in/";
+
+    document.querySelector("#github-repo").href =
+      "https://github.com/Bidyut163/Sochlo";
+  }
+
+  if (projectNo === 2) {
+    document.querySelector(".popup__img").src = "./img/popup-2.png";
+
+    document.querySelector(".popup__title").innerHTML = "Devconnector";
+
+    document.querySelector(".popup__tagline").innerHTML =
+      "A social neworking site";
+
+    document.querySelector(".popup__para").innerHTML =
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero,accusamus sequi. Eius totam impedit illum quasi! Dolor pariatur, corporis quibusdam eius unde aliquam possimus nemo! lorem!!";
+
+    document.querySelector("#site-link").href =
+      "https://powerful-citadel-60388.herokuapp.com/";
+
+    document.querySelector("#github-repo").href =
+      "https://github.com/Bidyut163/devconnector";
+  }
+
+  if (projectNo === 3) {
+    document.querySelector(".popup__img").src = "./img/popup-3.png";
+
+    document.querySelector(".popup__title").innerHTML = "Streamy";
+
+    document.querySelector(".popup__tagline").innerHTML = "A Crud React app";
+
+    document.querySelector(".popup__para").innerHTML =
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero,accusamus sequi. Eius totam impedit illum quasi! Dolor pariatur, corporis quibusdam eius unde aliquam possimus nemo! lorem!!";
+
+    document.querySelector("#site-link").href =
+      "https://github.com/Bidyut163/Streamy";
+
+    document.querySelector("#github-repo").href =
+      "https://github.com/Bidyut163/Streamy";
+  }
+
+  if (projectNo === 4) {
+    document.querySelector(".popup__img").src = "./img/popup-4.png";
+
+    document.querySelector(".popup__title").innerHTML = "LoopLab";
+
+    document.querySelector(".popup__tagline").innerHTML =
+      "A social neworking site";
+
+    document.querySelector(".popup__para").innerHTML =
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero,accusamus sequi. Eius totam impedit illum quasi! Dolor pariatur, corporis quibusdam eius unde aliquam possimus nemo! lorem!!";
+
+    document.querySelector("#site-link").href =
+      "https://github.com/Bidyut163/Looplab";
+
+    document.querySelector("#github-repo").href =
+      "https://github.com/Bidyut163/Looplab";
+  }
+
+  if (projectNo === 5) {
+    document.querySelector(".popup__img").src = "./img/popup-5.png";
+
+    document.querySelector(".popup__title").innerHTML = "Natours";
+
+    document.querySelector(".popup__tagline").innerHTML = "A trekking site";
+
+    document.querySelector(".popup__para").innerHTML =
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero,accusamus sequi. Eius totam impedit illum quasi! Dolor pariatur, corporis quibusdam eius unde aliquam possimus nemo! lorem!!";
+
+    document.querySelector("#site-link").href =
+      "https://github.com/Bidyut163/Natours";
+
+    document.querySelector("#github-repo").href =
+      "https://github.com/Bidyut163/Natours";
+  }
+
+  if (projectNo === 6) {
+    document.querySelector(".popup__img").src = "./img/popup-6.png";
+
+    document.querySelector(".popup__title").innerHTML = "YelpCamp";
+
+    document.querySelector(".popup__tagline").innerHTML = "A Camping site";
+
+    document.querySelector(".popup__para").innerHTML =
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero,accusamus sequi. Eius totam impedit illum quasi! Dolor pariatur, corporis quibusdam eius unde aliquam possimus nemo! lorem!!";
+
+    document.querySelector("#site-link").href =
+      "https://murmuring-meadow-11794.herokuapp.com/";
+
+    document.querySelector("#github-repo").href =
+      "https://github.com/Bidyut163/YelpCamp";
+  }
+}
